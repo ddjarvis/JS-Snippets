ef5e5cb4031058cd3a13535393bfdf718342e838
David Jarvis <ddjarvis@up.edu.ph>
2020-06-15 02:23:02 +0800

    Modded code[1] and updated meta[2].
    [1] Code Modification:
    Added return value <return.origin>
    
    [2] Meta Updates:
    - Added [Modifications] section and updated section information
    - Added [Parameters] subsection under [Syntax] section and updated subsection information
    - Added [Return Values] subsection under [Syntax] section and updated subsection information
    - Added [Examples] section and added 1 example to section

--- js/parseURL.js
+++ js/parseURL.js
@@ -1,17 +1,52 @@
 /*
+		~~==-== function parseURL() ==-==~~
 
--= Source =-
-Title: Parsing URLs in JavaScript
-Author: Cory LaViska (https://www.abeautifulsite.net/author/claviska)
-Link: https://www.abeautifulsite.net/parsing-urls-in-javascript
-Link (Archived): https://web.archive.org/web/20200614163246/https://www.abeautifulsite.net/parsing-urls-in-javascript
+	-= Source =-
+	Title: Parsing URLs in JavaScript
+	Author: Cory LaViska (https://www.abeautifulsite.net/author/claviska)
+	Link: https://www.abeautifulsite.net/parsing-urls-in-javascript
+	Link (Archived): https://web.archive.org/web/20200614163246/https://www.abeautifulsite.net/parsing-urls-in-javascript
 
--= Description =-
-This function takes a URL and returns its different components.
 
--= Syntax =-
-parseURL(string)
+	-= Description =-
+	This function takes a URL and returns its different components.
 
+	-= Modifications =-
+	> Added return value [parseURL().origin]
+
+	-= Syntax =-
+	parseURL(string)
+
+	Parameters:
+		<string> - A JavaScript string. Preferably a valid URL.
+
+	Return value(s):
+		<parseURL.hash>
+		<parseURL.host>
+		<parseURL.origin>
+		<parseURL.hostname>
+		<parseURL.pathname>
+		<parseURL.port>
+		<parseURL.protocol>
+		<parseURL.search>
+		<parseURL.searchObject>
+
+
+	-= Example(s) =-
+
+	Example 1:
+
+	var output = parseURL('https://example.com:8080/path?query=value#hashstring');
+
+	console.log(output.protocol);		// [protocol] :  https:
+	console.log(output.origin);			// [origin] :  https://example.com:8080
+	console.log(output.host);			// [host] :  example.com:8080
+	console.log(output.hostname);		// [hostname] :  example.com
+	console.log(output.port);			// [port] :  8080
+	console.log(output.pathname);		// [pathname] :  /path
+	console.log(output.search);			// [search] :  ?query=value
+	console.log(output.searchObject);	// [searchObject] :  [object Object]
+	console.log(output.hash);			// [hash] :  #hashstring
 */
 
 
@@ -29,6 +64,7 @@ function parseURL(url) {
     }
     return {
         protocol: parser.protocol,
+		origin: parser.origin,
         host: parser.host,
         hostname: parser.hostname,
         port: parser.port,
