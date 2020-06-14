858a34f2a6f401fda8c2609a89a82ee11d2a9443
David Jarvis <ddjarvis@up.edu.ph>
2020-06-15 00:24:16 +0800

    Create fileSafe.js

---
+++ js/fileSafe.js
@@ -0,0 +1,11 @@
+function fileSafe(input)
+{
+	var proc = input, output;
+	proc = proc.replace(/( +<+ +(?!.*>))|((?<!<.*) +>+ +)|( +:+ +)/g," - ");
+	proc = proc.replace(/([?*"]+)/g,"");
+	proc = proc.replace(/(<+(?!.*>))|((?<!<.*)>+)|((?<!<.*)(?<=>.*)>)|([/\\|]+)|(:+)/g," ");
+	proc = proc.replace(/</g,"(").replace(/>/g,")");
+	proc = proc.replace(/( {2,})/g," ");
+	output = proc.trim();
+	return output;
+}
