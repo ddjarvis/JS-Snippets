c3c0c555871cfbfdc74ac65729ef0e914f9416bd
David Jarvis <ddjarvis@up.edu.ph>
2020-12-15 20:41:57 -0600

    Update fileSafe.js

--- js/fileSafe.js
+++ js/fileSafe.js
@@ -3,7 +3,7 @@ function fileSafe(input)
 	var proc = input, output;
 	proc = proc.replace(/( +<+ +(?!.*>))|((?<!<.*) +>+ +)|(( ?:+ +)|( +:+ ?)|( +:+ +))/g," - ");
 	proc = proc.replace(/([?*"]+)/g,"");
-	proc = proc.replace(/(<+(?!.*>))|((?<!<.*)>+)|((?<!<.*)(?<=>.*)>)|([/\\|]+)|(:+)/g," ");
+	proc = proc.replace(/(<+(?!.*>))|((?<!<.*)>+)|((?<!<.*)(?<=>.*)>)|([\/\\|]+)|(:+)/g," ");
 	proc = proc.replace(/</g,"(").replace(/>/g,")");
 	proc = proc.replace(/( {2,})/g," ");
 	output = proc.trim();
