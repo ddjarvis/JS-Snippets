0f1c40e53d8ea6dad8be92ba8f012d4f74ae6837
David Jarvis <ddjarvis@up.edu.ph>
2021-07-22 15:36:58 +0800

    Create addCode.js

---
+++ js-functions/addCode.js
@@ -0,0 +1,16 @@
+function addCode(inputVar = "") {
+	if (!inputVar) {
+		return;
+	}
+	var s = document.createElement("script");
+	s.type = "text/javascript";
+	var c = "\n" + inputVar + "\n";
+	console.log(c);
+	try {
+		s.appendChild(document.createTextNode(c));
+		document.body.appendChild(s);
+	} catch (e) {
+		s.text = c;
+		document.body.appendChild(s);
+	}
+}
