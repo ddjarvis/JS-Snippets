e3ccce181ed83e3ec5b5d9e33162e00b223413b5
David Jarvis <ddjarvis@up.edu.ph>
2022-02-05 19:10:11 +0800

    Create addElement.js

---
+++ js-functions/addElement.js
@@ -0,0 +1,28 @@
+function addElement(elemType = '', elemId = '', elemText = '', parentElem = '', beforeElem = '') {
+  if (!elemId) {
+    elemId = 'my' + elemType.charAt(0).toUpperCase() + elemType.substr(1).toLowerCase();
+  }
+  if (!elemText) {
+    elemText = '&nbsp;';
+  }
+  if (elemText == "-") {
+    elemText = '';
+  }
+
+  if (!parentElem) {
+    parent.Elem = 'document.body'
+  }
+
+  var elem = document.createElement(elemType);
+  elem.id = elemId;
+
+  var txt = elemText;
+
+  elem.appendChild(document.createTextNode(txt));
+  if (!beforeElem) {
+    parentElem.appendChild(elem);
+  } else {
+    parentElem.insertBefore(elem, beforeElem)
+  }
+  return document.getElementById(elemId);
+}
