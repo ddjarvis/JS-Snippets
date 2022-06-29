6575abfc13b0c9b49e23841f4d85ed6c2f568ac1
David Jarvis <ddjarvis@up.edu.ph>
2022-06-29 11:31:00 +0800

    Update checkElement.js

--- js-functions/checkElement.js
+++ js-functions/checkElement.js
@@ -1,1 +1,14 @@
-
+function checkElement(selector,index,limit){
+	return new Promise((resolve,reject) => {
+	console.log(index);
+		index = index ?? '0';
+		let count = 0;
+		(function waitForFoo() {
+		const element = document.querySelectorAll(selector)[index];
+		if (element) return resolve(element);
+		if (limit && count > limit) return false;
+		count += 1;
+		setTimeout(waitForFoo, 50);
+		})();
+	});
+}
