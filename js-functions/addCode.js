8bc437300cb982e59fcc19d1344a0082c76f7795
David Jarvis <ddjarvis@up.edu.ph>
2022-10-29 21:01:51 +0800

    Update addCode.js

--- js-functions/addCode.js
+++ js-functions/addCode.js
@@ -4,6 +4,7 @@ function addCode(inputVar = "") {
 	}
 	var s = document.createElement("script");
 	s.type = "text/javascript";
+	s.dataset.label = 'Injected Code';
 	var c = "\n" + inputVar + "\n";
 	console.log(c);
 	try {
